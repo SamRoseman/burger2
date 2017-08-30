677ea31e01f7b4d19aa47c673536db1235060acf
Samantha Roseman <samantharoseman@Sams-MacBook-Pro.local>
2017-08-30 11:59:50 -0700

    jaws bug update

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,22 @@
 var Sequelize = require("sequelize");
 
-var sequelize = new Sequelize("burgers2_db", "root", "", {
-    host: "localhost",
-    dialect: "mysql",
-    pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-    }
-});
+var sequelize;
+
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+}
+else {
+    sequelize = new Sequelize("burgers2_db", "root", "",  {
+        host: "localhost",
+        dialect: "mysql",
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 10000
+        }
+    });
+}
+
 
 
 module.exports = sequelize;
